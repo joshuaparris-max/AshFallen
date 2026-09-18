a5dcb9fc315e013b422065296ea66ade773ad33f
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-09-19 07:30:05 +1000

    kernel: initialise native storage after paging handoff

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -10,6 +10,7 @@
 #include "paging.h"
 #include "serial.h"
 #include "shell.h"
+#include "storage_runtime.h"
 
 static boot_context_t boot_context;
 
@@ -30,6 +31,12 @@ static __attribute__((noreturn)) void kernel_after_paging(void) {
         halt_forever();
     }
     serial_write("JOSHOS_PAGING_PERMISSIONS_OK\n");
+
+    if (!storage_runtime_init(&boot_context)) {
+        serial_write("JOSHOS_ERROR_STORAGE_RUNTIME\n");
+        halt_forever();
+    }
+    serial_write("JOSHOS_STORAGE_RUNTIME_OK\n");
 
     gfx_init(&boot_context.framebuffer);
     desktop_layout_t layout = desktop_draw();
