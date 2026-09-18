5ee6c1c4cb78c370eee1a0cdbea3cea419fe6fe8
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-09-19 07:36:25 +1000

    kernel: add word and dword port I/O helpers

--- kernel/src/io.h
+++ kernel/src/io.h
@@ -13,4 +13,24 @@ static inline void outb(uint16_t port, uint8_t value) {
     __asm__ volatile ("outb %0, %1" :: "a"(value), "Nd"(port));
 }
 
+static inline uint16_t inw(uint16_t port) {
+    uint16_t value;
+    __asm__ volatile ("inw %1, %0" : "=a"(value) : "Nd"(port));
+    return value;
+}
+
+static inline void outw(uint16_t port, uint16_t value) {
+    __asm__ volatile ("outw %0, %1" :: "a"(value), "Nd"(port));
+}
+
+static inline uint32_t inl(uint16_t port) {
+    uint32_t value;
+    __asm__ volatile ("inl %1, %0" : "=a"(value) : "Nd"(port));
+    return value;
+}
+
+static inline void outl(uint16_t port, uint32_t value) {
+    __asm__ volatile ("outl %0, %1" :: "a"(value), "Nd"(port));
+}
+
 #endif
