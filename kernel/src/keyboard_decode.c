9f334410e67b03e5f77867a6ac8056e0d27b2c82
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-09-19 07:40:31 +1000

    fix: disambiguate shifted keyboard lookup table

--- kernel/src/keyboard_decode.c
+++ kernel/src/keyboard_decode.c
@@ -1,6 +1,6 @@
 #include "keyboard_decode.h"
 
-static char translate(uint8_t code, int shifted) {
+static char translate(uint8_t code, int shift_active) {
     static const char normal[58] = {
         0, 27, '1','2','3','4','5','6','7','8','9','0','-','=', '\b','\t',
         'q','w','e','r','t','y','u','i','o','p','[',']','\n',0,
