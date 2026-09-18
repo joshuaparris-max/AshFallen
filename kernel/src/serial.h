34c785b87a4abc859bdb8a11810db2a3907c8e88
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-09-18 21:46:06 +1000

    kernel: add exception IDT groundwork

--- kernel/src/serial.h
+++ kernel/src/serial.h
@@ -1,5 +1,10 @@
 #ifndef JOSHOS_SERIAL_H
 #define JOSHOS_SERIAL_H
+
+#include <stdint.h>
+
 void serial_init(void);
 void serial_write(const char *text);
+void serial_write_hex64(uint64_t value);
+
 #endif
